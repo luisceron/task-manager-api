0a74d73ab92955ea3566436171d55bb94986871a
Luis Ceron <luismarcoceron@gmail.com>
2017-08-12 10:08:58 -0300

    Adding create action to the users controllers with tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Users API', type: :request do
 
   before { host! 'api.taskmanager.dev' }
 
+
   describe 'GET /users/:id' do
     before do
       headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
@@ -28,6 +29,39 @@ RSpec.describe 'Users API', type: :request do
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
+      end
+    end
+  end
+
+  describe 'POST /users' do
+    before do
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+      post '/users', params: { user: user_params}, headers: headers
+    end
+
+    context 'when the request params are valid' do
+      let(:user_params) { attributes_for(:user) }
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns json data for the created user' do
+        user_response = JSON.parse(response.body)
+        expect(user_response['email']).to eq(user_params[:email])
+      end
+    end
+
+    context 'when the request params are invalid' do
+      let(:user_params) { attributes_for(:user, email: 'invalid_email@') }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns json data for errors' do
+        user_response = JSON.parse(response.body)
+        expect(user_response).to have_key('errors')
       end
     end
   end
