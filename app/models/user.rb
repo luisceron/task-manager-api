932c41d058994c416fffd0e19008117afaf5c5fc
Luis Ceron <luismarcoceron@gmail.com>
2017-08-16 18:34:26 -0300

    lesson 29 finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :auth_token, uniqueness: true #presence: true,
+
+  def info
+    "#{email} - #{created_at}"
+  end
 end
