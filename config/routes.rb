6dea43df8cba4a2562a1703d83d6df82c80c14d9
Luis Ceron <luismarcoceron@gmail.com>
2017-09-16 11:31:24 -0300

    Adding destroy action to the tasks controller with tests

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resources :sessions, only: [:create, :destroy]
       resources :users, only: [:show, :create, :update, :destroy]
-      resources :tasks, only: [:index, :show, :create, :update]
+      resources :tasks, only: [:index, :show, :create, :update, :destroy]
     end
   end
 
