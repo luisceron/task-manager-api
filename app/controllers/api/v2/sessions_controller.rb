3a0d8d291fadeca342ed25868e1a54d2bd4909bb
Luis Ceron <luismarcoceron@gmail.com>
2017-10-05 18:30:32 -0300

    Implementing JSONAPI.ORG patterns for sessions

--- app/controllers/api/v2/sessions_controller.rb
+++ app/controllers/api/v2/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V2::SessionsController < ApplicationController
+
   def create
     user = User.find_by(email: sessions_params[:email])
 
@@ -24,4 +25,5 @@ class Api::V2::SessionsController < ApplicationController
     def sessions_params
       params.require(:session).permit(:email, :password)
     end
+
 end
